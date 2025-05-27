adc8e242f2211356447674ed2a8407960cda9d5e
CodeNetJava <96720177+CodeNetJava@users.noreply.github.com>
2025-05-27 12:35:37 +0530

    added configservices

---
+++ services/discovery-server/src/test/java/com/sau/discovery_server/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sau.discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
